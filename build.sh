28df967648dd23c1384537d7a7f90449a289717a
Maxim Menshikov <maxim@menshikov.org>
2024-12-08 23:36:05 +0000

    Fail if NodeJS is not installed

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+function fail()
+{
+	echo $@ >&2
+	exit 1
+}
+
+which node || fail "NodeJS must be installed"
+
 new_secret="$(node -e "console.log(require('crypto').randomBytes(32).toString('hex'));")"
 cat docker-compose.changeme.yml | sed "s/GENIEACS_UI_JWT_SECRET=changeme/GENIEACS_UI_JWT_SECRET=${new_secret}/g" > docker-compose.yml
 docker build -t voost/genieacs:1.2.13 .
